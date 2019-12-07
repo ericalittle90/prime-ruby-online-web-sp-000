e6fe9d64992c94e57da47bdd95f985021dbd1513
Erica Little <eteney90@gmail.com>
2019-12-07 20:31:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 def prime?(num)
 if num <=1
-  then false
+  return false
 elsif (2..num-1).each do |num|
   num % num == 0
-  then true
+  return true
+end
 else
-  false
+  return false
 end
 end
 end
